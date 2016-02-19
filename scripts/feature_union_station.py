e600641d2507af0e5fb86c2efbcb0c943fa9cffa
Matthew Bender <benderm@gmail.com>
2016-02-18 23:09:54 -0700

    new features

--- scripts/feature_union_station.py
+++ scripts/feature_union_station.py
@@ -30,13 +30,31 @@ df_all = pd.concat((df_train, df_test), axis=0, ignore_index=True)
 df_all = pd.merge(df_all, df_pro_desc, how='left', on='product_uid')
 df_all = pd.merge(df_all, df_brand, how='left', on='product_uid')
 
+strNum = {'zero':0,'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9}
+
+
 def str_stem(s):
     if isinstance(s, str):
+        s = re.sub(r"(\w)\.([A-Z])", r"\1 \2", s) #Split words with a.A
         s = s.lower()
-        s = s.replace("-"," ") # character
-        s = s.replace(":"," ") # character
-        s = s.replace(";"," ") # character
+        s = s.replace("-"," ")
+        s = s.replace(":"," ")
+        s = s.replace(";"," ")
+        s = s.replace("  "," ")
+        s = s.replace(",","") #could be number / segment later
+        s = s.replace("$"," ")
+        s = s.replace("?"," ")
+        s = s.replace("-"," ")
+        s = s.replace("//","/")
+        s = s.replace("..",".")
+        s = s.replace(" / "," ")
+        s = s.replace(" \\ "," ")
+        s = s.replace("°"," degrees ")
+        s = s.replace("′′"," inches ")
+        s = s.replace("′"," feet ")
+        s = s.replace(" v "," volts ")
 
+        # slit dimentions
         s = s.replace(" x "," xby ")
         s = s.replace("*"," xby ")
         s = s.replace(" by "," xby")
@@ -61,40 +79,75 @@ def str_stem(s):
         s = s.replace("8x","8 xby ")
         s = s.replace("9x","9 xby ")
 
+        # regex replacements for measurements
         s = re.sub(r"([0-9]+)( *)(inches|inch|in|'')\.?", r"\1in. ", s)
-
         s = re.sub(r"([0-9]+)( *)(foot|feet|ft|')\.?", r"\1ft. ", s)
-
         s = re.sub(r"([0-9]+)( *)(pounds|pound|lbs|lb)\.?", r"\1lb. ", s)
-
         s = re.sub(r"([0-9]+)( *)(square|sq) ?\.?(feet|foot|ft)\.?", r"\1sq.ft. ", s)
-
+        s = re.sub(r"([0-9]+)( *)(cubic|cu) ?\.?(feet|foot|ft)\.?", r"\1cu.ft. ", s)
         s = re.sub(r"([0-9]+)( *)(gallons|gallon|gal)\.?", r"\1gal. ", s)
-
         s = re.sub(r"([0-9]+)( *)(ounces|ounce|oz)\.?", r"\1oz. ", s)
-
         s = re.sub(r"([0-9]+)( *)(centimeters|cm)\.?", r"\1cm. ", s)
-
         s = re.sub(r"([0-9]+)( *)(milimeters|mm)\.?", r"\1mm. ", s)
-
-        s = re.sub(r"([0-9]+)( *)(degrees|degree|degs|deg|°)\.?", r"\1deg. ", s)
-
+        s = re.sub(r"([0-9]+)( *)(degrees|degree|degs|deg)\.?", r"\1deg. ", s)
         s = re.sub(r"([0-9]+)( *)(volts|volt)\.?", r"\1volt. ", s)
-
         s = re.sub(r"([0-9]+)( *)(watts|watt)\.?", r"\1watt. ", s)
-
         s = re.sub(r"([0-9]+)( *)(amperes|ampere|amps|amp)\.?", r"\1amp. ", s)
 
+        # Spelling Mistakes
+        s = s.replace("toliet","toilet")
+        s = s.replace("airconditioner","air conditioner")
+        s = s.replace("vinal","vinyl")
+        s = s.replace("vynal","vinyl")
+        s = s.replace("skill","skil")
+        s = s.replace("snowbl","snow bl")
+        s = s.replace("plexigla","plexi gla")
+        s = s.replace("rustoleum","rust-oleum")
         s = s.replace("whirpool","whirlpool")
         s = s.replace("whirlpoolga", "whirlpool")
         s = s.replace("whirlpoolstainless","whirlpool stainless")
 
         s = s.replace("  "," ")
 
+        s = (" ").join([str(strNum[z]) if z in strNum else z for z in s.split(" ")])
         s = (" ").join([stemmer.stem(z) for z in s.split(" ")])
         return s.lower()
     else:
         return "null"
+
+def seg_words(str1, str2):
+    str2 = str2.lower()
+    str2 = re.sub("[^a-z0-9./]"," ", str2)
+    str2 = [z for z in set(str2.split()) if len(z)>2]
+    words = str1.lower().split(" ")
+    s = []
+    for word in words:
+        if len(word)>3:
+            s1 = []
+            s1 += segmentit(word,str2,True)
+            if len(s)>1:
+                s += [z for z in s1 if z not in ['er','ing','s','less'] and len(z)>1]
+            else:
+                s.append(word)
+        else:
+            s.append(word)
+    return (" ".join(s))
+
+def segmentit(s, txt_arr, t):
+    st = s
+    r = []
+    for j in range(len(s)):
+        for word in txt_arr:
+            if word == s[:-j]:
+                r.append(s[:-j])
+                #print(s[:-j],s[len(s)-j:])
+                s=s[len(s)-j:]
+                r += segmentit(s, txt_arr, False)
+    if t:
+        i = len(("").join(r))
+        if not i==len(st):
+            r.append(st[i:])
+    return r
 
 def str_common_word(str1, str2):
     words, cnt = str1.split(), 0
@@ -168,13 +221,22 @@ df_all['search_term'] = df_all['search_term'].map(lambda x:str_stem(x))
 df_all['product_title'] = df_all['product_title'].map(lambda x:str_stem(x))
 df_all['product_description'] = df_all['product_description'].map(lambda x:str_stem(x))
 df_all['brand'] = df_all['brand'].map(lambda x:str_stem(x))
+print("--- Stemming: %s minutes ---" % round(((time.time() - start_time)/60),2))
+df_all['product_info'] = df_all['search_term']+"\t"+df_all['product_title'] +"\t"+df_all['product_description']
+print("--- Prod Info: %s minutes ---" % round(((time.time() - start_time)/60),2))
 df_all['len_of_query'] = df_all['search_term'].map(lambda x:len(x.split())).astype(np.int64)
 df_all['len_of_title'] = df_all['product_title'].map(lambda x:len(x.split())).astype(np.int64)
 df_all['len_of_description'] = df_all['product_description'].map(lambda x:len(x.split())).astype(np.int64)
 df_all['len_of_brand'] = df_all['brand'].map(lambda x:len(x.split())).astype(np.int64)
-df_all['product_info'] = df_all['search_term']+"\t"+df_all['product_title'] +"\t"+df_all['product_description']
+print("--- Len of: %s minutes ---" % round(((time.time() - start_time)/60),2))
+df_all['search_term'] = df_all['product_info'].map(lambda x:seg_words(x.split('\t')[0],x.split('\t')[1]))
+#print("--- Search Term Segment: %s minutes ---" % round(((time.time() - start_time)/60),2))
 df_all['query_in_title'] = df_all['product_info'].map(lambda x:str_whole_word(x.split('\t')[0],x.split('\t')[1],0))
 df_all['query_in_description'] = df_all['product_info'].map(lambda x:str_whole_word(x.split('\t')[0],x.split('\t')[2],0))
+print("--- Query In: %s minutes ---" % round(((time.time() - start_time)/60),2))
+df_all['query_last_word_in_title'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0].split(" ")[-1],x.split('\t')[1]))
+df_all['query_last_word_in_description'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0].split(" ")[-1],x.split('\t')[2]))
+print("--- Query Last Word In: %s minutes ---" % round(((time.time() - start_time)/60),2))
 df_all['word_in_title'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0],x.split('\t')[1]))
 df_all['word_in_description'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0],x.split('\t')[2]))
 df_all['ratio_title'] = df_all['word_in_title']/df_all['len_of_query']
@@ -184,21 +246,16 @@ df_all['word_in_brand'] = df_all['attr'].map(lambda x:str_common_word(x.split('\
 df_all['ratio_brand'] = df_all['word_in_brand']/df_all['len_of_brand']
 df_brand = pd.unique(df_all.brand.ravel())
 d={}
-i = 1
+i = 1000
 for s in df_brand:
     d[s]=i
-    i+=1
+    i+=3
 df_all['brand_feature'] = df_all['brand'].map(lambda x:d[x])
 df_all['search_term_feature'] = df_all['search_term'].map(lambda x:len(x))
 
 df_all['similarity_in_description']=df_all['product_info'].map(lambda x:calculate_similarity(x.split('\t')[0],x.split('\t')[2]))
 df_all['similarity_in_title']=df_all['product_info'].map(lambda x:calculate_similarity(x.split('\t')[0],x.split('\t')[1]))
 
-df_all.head()
-print (df_all[:10])
-print("--- Features Set: %s minutes ---" % ((time.time() - start_time)/60))
-#df_all.to_csv('df_all.csv')
-#df_all = pd.read_csv('df_all.csv', encoding="ISO-8859-1", index_col=0)
 df_train = df_all.iloc[:num_train]
 df_test = df_all.iloc[num_train:]
 id_test = df_test['id']
@@ -209,7 +266,7 @@ print("--- Features Set: %s minutes ---" % round(((time.time() - start_time)/60)
 
 rfr = RandomForestRegressor(n_estimators = 150, random_state = 21337, verbose = 1)
 tfidf = TfidfVectorizer(ngram_range=(1, 1), stop_words='english')
-tsvd = TruncatedSVD(n_components=25, random_state = 21337)
+tsvd = TruncatedSVD(n_components=15, random_state = 21337)
 clf = pipeline.Pipeline([
         ('union', FeatureUnion(
                     transformer_list = [
@@ -230,7 +287,7 @@ clf = pipeline.Pipeline([
         ('rfr', rfr)])
 param_grid = {
                 'rfr__n_estimators' : [125],
-                'rfr__max_depth': [23],
+                'rfr__max_depth': [25],
                 'rfr__max_features' : [11,12]
              }
 model = grid_search.GridSearchCV(estimator = clf, param_grid = param_grid,
@@ -244,5 +301,5 @@ print(model.best_score_)
 
 y_pred = model.predict(X_test)
 print(len(y_pred))
-pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('../submissions/union_station_1.csv',index=False)
+pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('../submissions/union_station_4.csv',index=False)
 print("--- Training & Testing: %s minutes ---" % ((time.time() - start_time)/60))
