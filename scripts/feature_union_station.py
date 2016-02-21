6edc38bca7218ce90b87c004a101cd2bfa6cdb1d
Matthew Bender <benderm@gmail.com>
2016-02-20 17:04:58 -0700

    even more replacement

--- scripts/feature_union_station.py
+++ scripts/feature_union_station.py
@@ -35,13 +35,10 @@ strNum = {'zero':0,'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7
 
 def str_stem(s):
     if isinstance(s, str):
+        s = re.sub(r"(\w)\.([A-Z])", r"\1 \2", s) #Split words with a.A
         s = s.lower()
-        s = re.sub(r"(\w)\.([a-z])", r"\1 \2", s)
-        s = s.replace("-"," ")
-        s = s.replace(":"," ")
-        s = s.replace(";"," ")
         s = s.replace("  "," ")
-        s = s.replace(", "," ")
+        s = s.replace(",","") #could be number / segment later
         s = s.replace("$"," ")
         s = s.replace("?"," ")
         s = s.replace("-"," ")
@@ -49,61 +46,43 @@ def str_stem(s):
         s = s.replace("..",".")
         s = s.replace(" / "," ")
         s = s.replace(" \\ "," ")
-        s = s.replace("°"," degrees ")
+        s = s.replace("."," . ")
         s = s.replace("′′"," inches ")
         s = s.replace("′"," feet ")
-        s = s.replace(" v "," volts ")
-
-        # slit dimentions
-        s = s.replace(" x "," xby ")
-        s = s.replace("*"," xby ")
-        s = s.replace(" by "," xby ")
-        s = s.replace("x0"," xby 0")
-        s = s.replace("x1"," xby 1")
-        s = s.replace("x2"," xby 2")
-        s = s.replace("x3"," xby 3")
-        s = s.replace("x4"," xby 4")
-        s = s.replace("x5"," xby 5")
-        s = s.replace("x6"," xby 6")
-        s = s.replace("x7"," xby 7")
-        s = s.replace("x8"," xby 8")
-        s = s.replace("x9"," xby 9")
-        s = s.replace("0x","0 xby ")
-        s = s.replace("1x","1 xby ")
-        s = s.replace("2x","2 xby ")
-        s = s.replace("3x","3 xby ")
-        s = s.replace("4x","4 xby ")
-        s = s.replace("5x","5 xby ")
-        s = s.replace("6x","6 xby ")
-        s = s.replace("7x","7 xby ")
-        s = s.replace("8x","8 xby ")
-        s = s.replace("9x","9 xby ")
-
-        # regex replacements for measurements
+        s = re.sub(r"(^\.|/)", r"", s)
+        s = re.sub(r"(\.|/)$", r"", s)
+        s = re.sub(r"([0-9])([a-z])", r"\1 \2", s)
+        s = re.sub(r"([a-z])([0-9])", r"\1 \2", s)
+        s = s.replace(" x "," xbi ")
+        s = re.sub(r"([a-z])( *)\.( *)([a-z])", r"\1 \4", s)
+        s = re.sub(r"([a-z])( *)/( *)([a-z])", r"\1 \4", s)
+        s = s.replace("*"," xbi ")
+        s = s.replace(" by "," xbi ")
+        s = re.sub(r"([0-9])( *)\.( *)([0-9])", r"\1.\4", s)
         s = re.sub(r"([0-9]+)( *)(inches|inch|in|'')\.?", r"\1in. ", s)
         s = re.sub(r"([0-9]+)( *)(foot|feet|ft|')\.?", r"\1ft. ", s)
         s = re.sub(r"([0-9]+)( *)(pounds|pound|lbs|lb)\.?", r"\1lb. ", s)
         s = re.sub(r"([0-9]+)( *)(square|sq) ?\.?(feet|foot|ft)\.?", r"\1sq.ft. ", s)
         s = re.sub(r"([0-9]+)( *)(cubic|cu) ?\.?(feet|foot|ft)\.?", r"\1cu.ft. ", s)
-        s = re.sub(r"([0-9]+)( *)(gallons|gallon|gal)\.?", r"\1gal. ", s)
+        s = re.sub(r"([0-9]+)( *)(gallons|gallon|gals|gal)\.?", r"\1gal. ", s)
         s = re.sub(r"([0-9]+)( *)(ounces|ounce|oz)\.?", r"\1oz. ", s)
-        s = re.sub(r"([0-9]+)( *)(centimeters|centimeter|cm)\.?", r"\1cm. ", s)
-        s = re.sub(r"([0-9]+)( *)(milimeters|millimeters|millimeter|mm)\.?", r"\1mm. ", s)
-        s = re.sub(r"([0-9]+)( *)(degrees|degree|degs|deg)\.?", r"\1deg. ", s)
+        s = re.sub(r"([0-9]+)( *)(centimeters|cm)\.?", r"\1cm. ", s)
+        s = re.sub(r"([0-9]+)( *)(millimeters|milimeters|mm)\.?", r"\1mm. ", s)
+        s = s.replace("°"," degrees ")
+        s = re.sub(r"([0-9]+)( *)(degrees|degree)\.?", r"\1deg. ", s)
+        s = s.replace(" v "," volts ")
         s = re.sub(r"([0-9]+)( *)(volts|volt)\.?", r"\1volt. ", s)
         s = re.sub(r"([0-9]+)( *)(watts|watt)\.?", r"\1watt. ", s)
-        s = re.sub(r"([0-9]+)( *)(amperes|ampere|ampère|amps|amp)\.?", r"\1amp. ", s)
-
-        s = s.replace(" . "," ")
+        s = re.sub(r"([0-9]+)( *)(amperes|ampere|amps|amp)\.?", r"\1amp. ", s)
         s = s.replace("  "," ")
+        s = s.replace(" . "," ")
+        #s = (" ").join([z for z in s.split(" ") if z not in stop_w])
         s = (" ").join([str(strNum[z]) if z in strNum else z for z in s.split(" ")])
         s = (" ").join([stemmer.stem(z) for z in s.split(" ")])
 
-        # Spelling Mistakes
         s = s.lower()
         s = s.replace("toliet","toilet")
         s = s.replace("airconditioner","air conditioner")
-        s = s.replace("aircondition","air conditioner")
         s = s.replace("condtion","condition")
         s = s.replace("vinal","vinyl")
         s = s.replace("vynal","vinyl")
@@ -112,9 +91,8 @@ def str_stem(s):
         s = s.replace("plexigla","plexi gla")
         s = s.replace("rustoleum","rust-oleum")
         s = s.replace("whirpool","whirlpool")
-        s = s.replace("whirlpoolga", "whirlpool")
+        s = s.replace("whirlpoolga", "whirlpool ga")
         s = s.replace("whirlpoolstainless","whirlpool stainless")
-
         return s
     else:
         return "null"
@@ -290,9 +268,9 @@ clf = pipeline.Pipeline([
                 )),
         ('rfr', rfr)])
 param_grid = {
-                'rfr__n_estimators' : [125],
-                'rfr__max_depth': [25],
-                'rfr__max_features' : [11,12]
+                'rfr__n_estimators' : [123,125,127],
+                'rfr__max_depth': [26],
+                'rfr__max_features' : [11]
              }
 model = grid_search.GridSearchCV(estimator = clf, param_grid = param_grid,
     cv = 10, verbose = 250, scoring=RMSE)
@@ -305,5 +283,5 @@ print(model.best_score_)
 
 y_pred = model.predict(X_test)
 print(len(y_pred))
-pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('../submissions/union_station_7.csv',index=False)
+pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('../submissions/union_station_10.csv',index=False)
 print("--- Training & Testing: %s minutes ---" % ((time.time() - start_time)/60))
